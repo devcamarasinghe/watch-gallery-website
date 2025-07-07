f40eab695736b8027b762db6ddc98d298646afbd
Chanaka Amarasinghe <amarasinghegcj@gmail.com>
2025-07-07 11:31:58 +0530

    'toast-message'

---
+++ src/utils/toast.js
@@ -0,0 +1,29 @@
+// src/utils/toast.js
+import toast from 'react-hot-toast';
+
+export const showToast = {
+  success: (message) => toast.success(message, {
+    style: {
+      borderRadius: '20px',
+      background: '#333',
+      color: '#fff',
+    },
+    iconTheme: {
+      primary: '#fff',
+      secondary: '#4BB543',
+    },
+  }),
+  
+  error: (message) => toast.error(message, {
+    style: {
+      background: '#FF3333',
+      color: '#fff',
+    },
+  }),
+  
+  promise: (promise, messages) => toast.promise(promise, messages),
+  
+  loading: (message) => toast.loading(message),
+  
+  dismiss: () => toast.dismiss(),
+};
