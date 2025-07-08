e06dff8f9a1c144cb668375127a41a00392bf0f9
Chanaka Amarasinghe <amarasinghegcj@gmail.com>
2025-07-08 12:14:05 +0530

    config changes

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBDthcjt96FQoGJJY6ORNErKJCXO5kC-pI",
-    authDomain: "luxwatch-2025.firebaseapp.com",
-    projectId: "luxwatch-2025",
-    storageBucket: "luxwatch-2025.firebasestorage.app",
-    messagingSenderId: "385870903994",
-    appId: "1:385870903994:web:fc74598d4215814aa46083",
-    measurementId: "G-WEKSJ411T5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
